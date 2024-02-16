2890c4fab7cc1b9712d30523ef89ccf2ff6ef312
unknown <s22110352@student.unklab.ac.id>
2024-02-16 14:59:01 +0800

    adding exercise-rn #3

--- App.tsx
+++ App.tsx
@@ -1,99 +1,92 @@
 import {
-  Button,
   Text,
-  View,
-  Image,
   ScrollView,
   TextInput,
   StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
 import React from 'react';
-import Download from './aset/ferret.jpg';
-import Photo from './aset/hamster.jpg';
 
 const App = () => {
   return (
     <ScrollView style={style.body}>
-      <Text style={style.hello}>Hello!</Text>
+      <Text style={style.welcome}>Welcome</Text>
 
+      <Text style={style.uname}>Username</Text>
       <TextInput
         style={style.inputcollumn}
-        placeholder="Please enter your Username"
-        placeholderTextColor="#675e60"
-      />
-      <TextInput
-        style={style.inputcollumn}
-        placeholder="Please enter your Password"
-        placeholderTextColor="#675e60"
+        placeholder=" Masukan username anda"
+        placeholderTextColor="black"
         secureTextEntry
       />
-      <View style={style.container}>
-        <View style={[style.box, {backgroundColor: '#fc819c'}]} />
-        <View style={[style.box, {backgroundColor: '#f53a63'}]} />
-        <View style={[style.box, {backgroundColor: '#e90f3f'}]} />
-      </View>
-      <View style={style.buttonlog}>
-        <Button title="Login" color="#f45074" />
-      </View>
-      <Image style={style.ferret} source={Download}/>
-      <Image style={style.hamster} source={Photo}/>
+      <Text style={style.pw}>Password</Text>
+      <TextInput
+        style={style.inputcollumn}
+        placeholder=" Masukan password anda"
+        placeholderTextColor="black"
+        secureTextEntry
+      />
+      <TouchableOpacity style={style.touchableOpacity}>
+        <Text style={style.touchableText}>Sign In</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
 
 const style = StyleSheet.create({
   body: {
-    backgroundColor: '#fdb0c1',
-  },
-  ferret: {
-    paddingTop: 150,
-    marginTop: 50,
-    width: 170,
-    height: 130,
-    alignSelf: 'center',
-    borderRadius: 30,
-  },
-  hamster: {
-    paddingTop: 150,
-    marginTop: 50,
-    width: 170,
-    height: 130,
-    alignSelf: 'center',
-    borderRadius: 30,
-  },
-  container: {
-    flexDirection: 'row',
-    marginTop: 20,
-    justifyContent: 'space-around',
-    marginBottom: 80,
-    borderRadius: 15,
-  },
-  box: {
-    marginTop: 5,
-    width: 130,
-    height: 100,
-    borderRadius: 60,
+    backgroundColor: 'white',
+    flex: 1,
   },
   inputcollumn: {
-    backgroundColor: '#f8d1d9',
-    borderColor: 'white',
-    borderWidth: 3,
-    margin: 40,
-    marginTop: 20,
+    borderColor: 'black',
+    borderWidth: 2,
+    margin: 10,
+    padding: 10,
     borderRadius: 10,
-    textAlign: 'center',
   },
-  buttonlog: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+  welcome: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    borderColor: 'black',
+    color: 'black',
+    textAlign: 'left',
+    borderRadius: 50,
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 15,
   },
-  hello: {
-    fontSize: 80,
+  touchableOpacity: {
+    backgroundColor: '#ff6600',
+    alignSelf: 'center',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 35,
+    height: 45,
+    width: 380,
+  },
+  touchableText: {
+    color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
-    color: 'white',
-    lineHeight: 200,
-    fontFamily: 'serif',
+  },
+  pw: {
+    color: '#0d0d0d',
+    textAlign: 'left',
+    borderColor: 'black',
+    fontSize: 20,
+    marginTop: 10,
+    marginLeft: 15,
+    borderRadius: 50,
+  },
+  uname: {
+    color: '#0d0d0d',
+    textAlign: 'left',
+    borderColor: 'black',
+    fontSize: 20,
+    marginTop: 10,
+    marginLeft: 15,
+    borderRadius: 50,
   },
 });
 export default App;
